78937987acdb2fba8b10ea217cc10be7c3beb955
Fridberg <fridberg.traustason@gmail.com>
2016-11-29 00:46:18 +0000

    Betri comment á config/passport

--- app/config/passport.js
+++ app/config/passport.js
@@ -1,41 +1,29 @@
 var FacebookStrategy = require('passport-facebook').Strategy;
-// load up the user model
 var User = require('../models/user');
-
-// load the auth variables
 var configAuth = require('./auth');
 
 module.exports = (passport)=>{
 
-    // used to serialize the user for the session
+    // serialize-ar userinn í session
     passport.serializeUser((user, done)=>{
         done(null, user.ID);
     });
 
-    // used to deserialize the user
     passport.deserializeUser((id, done)=>{
         User.findById(id, function(err, user) {
             done(err, user);
         });
     });
 
-    // code for login (use('local-login', new LocalStategy))
-    // code for signup (use('local-signup', new LocalStategy))
-
-    // =========================================================================
-    // FACEBOOK ================================================================
-    // =========================================================================
+    // Pullum auth upplýsingar sem við stillum í  ./auth
     passport.use(new FacebookStrategy({
-
-        // pull in our app id and secret from our auth.js file
         clientID        : configAuth.facebookAuth.clientID,
         clientSecret    : configAuth.facebookAuth.clientSecret,
         callbackURL     : configAuth.facebookAuth.callbackURL,
-        profileFields: ['id', 'emails', 'name'] //This
-
+        profileFields: ['id', 'emails', 'name']
     },
 
-    // facebook will send back the token and profile
+    // runnað þegar facebook hefur skilað okkur upplýsingum
     (token, refreshToken, profile, done)=>{
         // Finnum notendan í facebook töfluni
         User.findOne(profile.id, 'userFacebook', function(err, user) {
@@ -46,31 +34,34 @@ module.exports = (passport)=>{
 
             // Loggum inn notenda ef hann finnst
             if (user) {
-                return done(null, user); // user found, return that user
+                return done(null, user);
             } else {
-                // if there is no user found with that facebook id, create them
+                // Ef enginn notandi finnst, búum hann til
                 let newUser = {};
                 let fbUser = {};
                 let name =  (profile.name.givenName || '') + ' ' +
                             (profile.name.middleName || '') + ' ' +
                             (profile.name.familyName || '');
 
-                // set all of the facebook information in our user model
-                fbUser.ID    = profile.id; // set the users facebook id
-                fbUser.token = token; // we will save the token that facebook provides to the user
-                fbUser.name  =  name; // look at the passport user profile to see how names are returned
-                fbUser.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+                // stilla allar upplýsingar frá facebook sem við sendum á modelið
+                fbUser.ID    = profile.id;
+                fbUser.token = token;
+                fbUser.name  =  name;
+                fbUser.email = profile.emails[0].value;
 
+                //Notendaupplýsingar uppfærðar
                 newUser.name = name;
                 newUser.email = fbUser.email;
 
+                //setjum inn userinn í db
                 User.add(newUser, (error, result)=>{
                   if(error){
                     return done(error);
                   }
 
-                  newUser.ID = result.insertId;
+                  newUser.ID = result.insertId; //Sækjum id eftir insert
 
+                  //Bætum fb login upplýsingum á user og tengjum hann við fb.
                   User.addToFacebook(newUser.ID, fbUser, (error, result)=>{
                     if(error){
                       return done(error)
